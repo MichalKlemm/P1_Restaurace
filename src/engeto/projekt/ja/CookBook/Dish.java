8413cdb06e9779ebaef784c6db3b78fe6fc52dae
MichalKlemm <Klemmmichal@lgmail.com>
2024-03-12 18:56:08 +0100

    Vytvoření třídy Dish
    pridání konstruktoru
    getters & setters
    pridání metody pro kontrolu správnosti vstupů

---
+++ src/engeto/projekt/ja/CookBook/Dish.java
@@ -0,0 +1,54 @@
+package engeto.projekt.ja.CookBook;
+
+import java.math.BigDecimal;
+
+public class Dish {
+
+    private String title;
+    private BigDecimal price;
+    private Integer preparationTime;
+    private String image;
+
+    public Dish(String title, BigDecimal price, Integer preparationTime, String image) {
+        this.title = title;
+        this.price = price;
+        this.preparationTime = preparationTime > 0 ? preparationTime : 1;
+        this.image = image != null && !image.isEmpty() ? image : "blank.png";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(Integer preparationTime) {
+        this.preparationTime = preparationTime > 0 ? preparationTime : 1;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image != null && !title.isEmpty() ? image : "blank.png";
+    }
+    public boolean isValid() {
+        return title != null && !title.isEmpty() && price != null && price.compareTo(BigDecimal.ZERO) >= 0
+                && preparationTime != null && preparationTime > 0 && image != null && !image.isEmpty();
+    }
+}
