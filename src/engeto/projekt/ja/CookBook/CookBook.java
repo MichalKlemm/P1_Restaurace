867d17340d1ce88e43fea7a58650e0aa8c7a42d2
MichalKlemm <Klemmmichal@lgmail.com>
2024-03-12 19:04:12 +0100

    Vytvoření třídy CookBook
    Vytvoření Listu
    přidání metody pro:
    pridani receptu
    odebrání receptu
    nalezeni receptu podle nazvu
    
    + getter pro seznam receptu

--- src/engeto/projekt/ja/CookBook/CookBook.java
+++ src/engeto/projekt/ja/CookBook/CookBook.java
@@ -1,4 +1,31 @@
 package engeto.projekt.ja.CookBook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CookBook {
+
+    private List<Dish> dishes;
+
+    public CookBook(){
+        this.dishes = new ArrayList<>();
+    }
+    public void addDish(Dish dish){
+        dishes.add(dish);
+    }
+    public void removeDish(Dish dish){
+        dishes.remove(dish);
+    }
+
+    public Dish findDishByTitle(String title){
+        for (Dish dish : dishes){
+            if (dish.getTitle().equals(title)){
+                return dish;
+            }
+        }
+        return null;
+    }
+    public List<Dish> getDishes(){
+        return dishes;
+    }
 }
